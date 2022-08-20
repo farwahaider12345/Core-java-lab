b539359d9100042b8d764651ced6c19d14e3ac40
farwahaider12345 <111423158+farwahaider12345@users.noreply.github.com>
2022-08-20 12:30:48 +0400

    Add files via upload

---
+++ CheckInput.java
@@ -0,0 +1,51 @@
+/*
+Program : Java program to check the input vowel,consonant,number special character to print its ascii value(Lab session)
+@author : Farwa Haider 
+@Date : 18 August 2022
+*/
+
+class CheckInput
+{
+   static void asciiValue()
+   {  
+   
+   int marks= Integer.parseInt(args[0]);  // string to integer 
+   char= Vowels ;
+   
+   if 
+   char= 'a'   
+     
+    {  
+        case 'a':   
+            System.out.println("Vowel");  
+            break;  
+        case 'e':   
+            System.out.println("Vowel");  
+            break;  
+        case 'i':   
+            System.out.println("Vowel");  
+            break;  
+        case 'o':   
+            System.out.println("Vowel");  
+            break;  
+        case 'u':   
+            System.out.println("Vowel");  
+            break; 
+        default:   
+            System.out.println("Consonant");  
+    }  
+	//ascii value of a
+	char c1='a'
+	int num=c1;
+	System.out.println("The value of "+cl+ "is:"num);
+
+
+										   
+   
+}
+    public static void main (String args[])
+	{
+	asciiValue();
+	}
+   
+}
